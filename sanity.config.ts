08ce9718ee7d3358dbb79850bd29c9766f8cd413
10407512 <10407512@ucn.dk>
2023-06-17 22:17:29 +0200

    Adding Sanity schema
    Adding schema for project

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import schemas from "./sanity/schemas";
 
 const config = defineConfig({
   projectId: "u3ww61mn",
@@ -8,6 +9,7 @@ const config = defineConfig({
   apiVersion: "2023-06-17",
   basePath: "/admin",
   plugins: [deskTool()],
+  schema: { types: schemas },
 });
 
 export default config;
