eb296115515b5fa3bd025503706a1a2652df1feb
10407512 <10407512@ucn.dk>
2023-06-19 15:02:02 +0200

    Added Project page and fetch function getProject
    Noticed redundant code in the fetch methods getProject and getProjects.

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -20,3 +20,23 @@ export async function getProjects(): Promise<Project[]> {
     }`
   );
 }
+
+export async function getProject(slug: string): Promise<Project> {
+  const client = createClient({
+    projectId: "u3ww61mn",
+    dataset: "production",
+    apiVersion: "2023-06-17",
+  });
+  return client.fetch(
+    groq`*[_type == "project" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`,
+    { slug }
+  );
+}
