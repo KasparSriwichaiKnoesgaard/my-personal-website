639d2b033606d43f225855083a13b87fcda5acd9
10407512 <10407512@ucn.dk>
2023-06-19 02:38:37 +0200

    Displaying my projects on the Home page
    Error in Home page: Parameter 'project' implicitly has an 'any' type.

---
+++ sanity/sanity-utils.ts
@@ -0,0 +1,22 @@
+import { Project } from "@/types/Project";
+import { createClient, groq } from "next-sanity";
+
+export async function getProjects() {
+  const client = createClient({
+    projectId: "u3ww61mn",
+    dataset: "production",
+    apiVersion: "2023-06-17",
+  });
+
+  return client.fetch(
+    groq`*[_type == "project"]{
+        _id,
+        _createdAt,
+        name,
+        "slug": slug.current,
+        "image": image.asset->url,
+        url,
+        content
+    }`
+  );
+}
