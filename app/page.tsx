43cf7b6d9a1c7bf20024bbeffc30e11f74774f57
10407512 <10407512@ucn.dk>
2023-06-19 14:40:47 +0200

    Adding links to my projects
    Added hover with transition to the grid.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { getProjects } from "@/sanity/sanity-utils";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   const projects = await getProjects();
@@ -15,9 +16,10 @@ export default async function Home() {
       <h2 className="mt-14 font-bold text-gray-700 text-3xl">My Projects</h2>
       <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects.map((project) => (
-          <div
+          <Link
+            href={`/projects/${project.slug}`}
             key={project._id}
-            className="border-2 border-gray-500 rounded-lg p-2"
+            className="border-2 border-gray-500 rounded-lg p-2 hover:scale-105 hover:border-blue-500 transition"
           >
             {project.image ? (
               <Image
@@ -27,7 +29,7 @@ export default async function Home() {
                 height={300}
                 className="object-cover rounded-lg border border-gray-500"
               />
-            ): (
+            ) : (
               <div className="flex items-center justify-center h-40 bg-gray-200 rounded-lg">
                 No image available
               </div>
@@ -35,7 +37,7 @@ export default async function Home() {
             <div className="mt-2 font-extrabold bg-gradient-to-r from-blue-500 via-lime-500 to-green-700 bg-clip-text text-transparent">
               <div className="w-max">{project.name}</div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
